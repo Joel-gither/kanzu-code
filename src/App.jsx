9e614771def59c18f9e7d367ed3625bc72696b74
icongraphics45@gmail.com <IconGraphics>
2025-06-09 13:00:21 +0300

    tailwind config

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,8 @@
+import React from 'react'
 
-import './App.css'
-
-function App() {
-   
-
+const App = () => {
   return (
-    <>
-       
-    </>
+    <div>App</div>
   )
 }
 
---
+++ tailwind.config.js
@@ -0,0 +1,40 @@
+// Example `tailwind.config.js` file
+import { coolGray, lightBlue, rose, fuchsia } from 'tailwindcss/colors';
+
+export const theme = {
+  colors: {
+    gray: coolGray,
+    blue: lightBlue,
+    red: rose,
+    pink: fuchsia,
+  },
+  screens: {
+    sm: "640px",
+    // => @media (min-width: 640px) { ... }
+    md: "768px",
+    // => @media (min-width: 768px) { ... }
+    lg: "1024px",
+    // => @media (min-width: 1024px) { ... }
+    xl: "1280px",
+  },
+  fontFamily: {
+    sans: ['Graphik', 'sans-serif'],
+    serif: ['Merriweather', 'serif'],
+  },
+  extend: {
+    spacing: {
+      '128': '32rem',
+      '144': '36rem',
+    },
+    borderRadius: {
+      '4xl': '2rem',
+    }
+  }
+};
+export const plugins = [];
+export const variants = {
+  extend: {
+    borderColor: ['focus-visible'],
+    opacity: ['disabled'],
+  }
+};
